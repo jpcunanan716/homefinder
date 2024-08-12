5b4e144e703a27ae25f63342a27a1744a6d0607b
jpcunanan716 <jpcunanan716@gmail.com>
2024-08-12 11:29:09 +0800

    finished revamping database, add login next step

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,4 @@ Route::get('/', function () {
 
 Route::get('/home', [HouseController::class, 'home'])->name('index');
 Route::get('/listing', [HouseController::class, 'listing'])->name('createlisting');
-Route::post('/home', [HouseController::class, 'save'])->name('save');
+Route::post('/home', [HouseController::class, 'home'])->name('save');
